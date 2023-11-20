11487a46e61c486dd5411661c4c9c3f9010111ba
Eddie Sandler <ed.sandler@gmail.com>
2023-11-20 18:07:09 -0500

    form handler working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template,  redirect, flash, session
+from flask import Flask, request, render_template,  redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db,  connect_db ,Pet
+from forms import NewPetForm
 
 
 app = Flask(__name__)
@@ -20,3 +21,21 @@ connect_db(app)
 def show_pets():
     pets=Pet.query.all()
     return render_template('pet_list.html',pets=pets)
+
+@app.route('/add',methods=['GET','POST'])
+def add_pet_form():
+    form=NewPetForm()
+    if form.validate_on_submit():
+        pet_name=form.pet_name.data
+        species=form.species.data
+        photo_url=form.photo_url.data
+        age=form.age.data
+        notes=form.notes.data
+
+        pet = Pet(name=pet_name, species=species, photo_url=photo_url,age=age,notes=notes)
+        db.session.add(pet)
+        db.session.commit()
+        flash(f' Added {pet.name} the {species}')
+        return redirect('/add')
+    else:
+        return render_template('add_pet_form.html',form=form)
--- forms.py
+++ forms.py
@@ -1,3 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
 from wtforms.validators import InputRequired, Email, Optional
+
+
+class NewPetForm(FlaskForm):
+    """form for adding pets"""
+    pet_name=StringField("Pet Name")
+    species=StringField("Species")
+    photo_url=StringField("Photo URL")
+    age=IntegerField("Age")
+    notes=StringField("Notes")
