97e6b8b88ac4289cf2288ae101b6bc376cbbaa10
Eddie Sandler <ed.sandler@gmail.com>
2023-11-20 18:36:49 -0500

    validation added

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
-from wtforms.validators import InputRequired, Email, Optional
+from wtforms.validators import InputRequired, Email, Optional,URL,AnyOf,NumberRange
 
 
 class NewPetForm(FlaskForm):
     """form for adding pets"""
     pet_name=StringField("Pet Name")
-    species=StringField("Species")
-    photo_url=StringField("Photo URL")
-    age=IntegerField("Age")
+    species=StringField("Species",validators=[AnyOf(values=['cat','dog','porcupine'])])
+
+    photo_url=StringField("Photo URL",validators=[URL(require_tld=False,message=None)])
+    age=IntegerField("Age",validators=[NumberRange(min=0,max=30)])
     notes=StringField("Notes")
